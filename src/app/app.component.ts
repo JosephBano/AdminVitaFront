747896bcf98c4d9158316c8d87b994bc257cb27c
Joseph Bano <andres.jbo2003@gmail.com>
2025-01-30 23:28:28 -0500

    Integration Tailwind and ngPrime on code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilterMatchMode } from 'primeng/api';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'AdminVitaFront';
+export class AppComponent implements OnInit{
+  constructor (private primeng: PrimeNG) {}
+  ngOnInit(): void {
+    this.primeng.ripple.set(true);
+    this.primeng.zIndex = {
+      modal:1100,
+      overlay:1100,
+      menu:1100,
+      tooltip:1100,
+    };
+    this.primeng.filterMatchModeOptions = {
+      text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
+      numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
+      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
+  };
+  }
 }
