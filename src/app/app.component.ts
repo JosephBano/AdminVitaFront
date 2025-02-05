c3ffa6f73efbb3bfbeda5388e4e7bb5218ff8a49
Joseph Bano <andres.jbo2003@gmail.com>
2025-02-05 11:50:31 -0500

    simplification of app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FilterMatchMode } from 'primeng/api';
-import { PrimeNG } from 'primeng/config';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterModule],
+    template: `<router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit{
-  constructor (private primeng: PrimeNG) {}
-  ngOnInit(): void {
-    this.primeng.ripple.set(true);
-    this.primeng.zIndex = {
-      modal:1100,
-      overlay:1100,
-      menu:1100,
-      tooltip:1100,
-    };
-    this.primeng.filterMatchModeOptions = {
-      text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
-      numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
-      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
-  };
-  }
-}
+export class AppComponent {}
