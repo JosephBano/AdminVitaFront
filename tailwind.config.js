117a52cd84a4aafe07c7c989bec77fa8ee2e7327
Joseph Bano <andres.jbo2003@gmail.com>
2025-02-05 11:48:38 -0500

    New variables and chanto sccs to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,scss,css}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1F295B',
+        secondary: '#C4A857',
+      },
+    },
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1920px'
+    }
   },
   plugins: [],
 }
