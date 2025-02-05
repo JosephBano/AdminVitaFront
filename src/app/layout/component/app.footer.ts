194f1c3fe52c772d23f0e9d59b984f961bf8cdcc
Joseph Bano <andres.jbo2003@gmail.com>
2025-02-05 11:50:05 -0500

    dashboard layout

---
+++ src/app/layout/component/app.footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-footer',
+    template: `<div class="layout-footer">
+        VITA by Instituto Superior Tecnológico Mayor Pedro Traversarí
+    </div>`
+})
+export class AppFooter {}
