f2bca443f797941d0db3793d9206a8972dcdf50a
Joseph Bano <andres.jbo2003@gmail.com>
2025-02-05 11:52:28 -0500

    change routes name, and add httpCliente provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+//import MyPreset from '../assets/util/Preset';
+import { provideHttpClient } from '@angular/common/http';
+import { appRoutes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
+    provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
+    provideAnimations(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+         preset: Aura,
+        //preset: MyPreset,
         options: {
-          prefix: 'p',
-          darkModeSelector: 'system',
-          cssLayer: false
+          darkModeSelector: '.my-app-dark'
+          // global styles
         }
       }
-    })
+    }),
+    provideHttpClient(),
   ]
 };
