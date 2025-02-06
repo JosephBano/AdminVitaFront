1a00874e77be717219bff26ba5fcfd5ac16fdb2c
Joseph Bano <andres.jbo2003@gmail.com>
2025-02-06 07:12:28 -0500

    routing app for dashboard and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './views/auth/login/login.component';
-import { HomeComponent } from './views/pages/home/home.component';
 import { AppLayout } from './layout/component/app.layout';
+import { HomeComponent } from './views/dashboard/home/home.component';
+import { OrdenTrabajoComponent } from './views/dashboard/orden-trabajo/orden-trabajo.component';
 
 export const appRoutes: Routes = [
-    {
-        path: '',
-        redirectTo: 'auth',
-        pathMatch: 'full'
-    },
-    {
-        path: 'auth',
-        component: LoginComponent
-    },
-    {
-        path: 'home',
-        component: AppLayout,
+    {   path: '', component: LoginComponent },
+    {   path: 'panel', component: AppLayout,
         children: [
-            {path: '', component: HomeComponent},
-        ]
-    }
+            { path: '', component: HomeComponent},
+            { path: 'OrdenTrabajo', component: OrdenTrabajoComponent}
+        ]},
+    {   path: '**', redirectTo: '', pathMatch: 'full'   },
 ];
