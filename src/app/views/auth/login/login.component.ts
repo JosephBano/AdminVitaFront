307436bbb5a66e5cccf22089256d464d46293951
Joseph Bano <andres.jbo2003@gmail.com>
2025-02-12 14:50:58 -0500

    Better login, spinner loading

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -9,11 +9,23 @@ import { RippleModule } from 'primeng/ripple';
 import { Router } from '@angular/router';
 import { NgIf, NgClass } from '@angular/common';
 import { AuthService } from '../service/auth.service';
+import { ProgressSpinner } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, NgIf, NgClass],
+  imports: [
+    ButtonModule,
+    CheckboxModule, 
+    InputTextModule, 
+    PasswordModule, 
+    FormsModule, 
+    RouterModule, 
+    RippleModule, 
+    NgIf, 
+    NgClass,
+    ProgressSpinner],
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -22,6 +34,9 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   flag: boolean = false;
+
+  buttonContent: string = 'INGRESAR';
+  flagSpinner: boolean = false;
   
   constructor(private authService: AuthService, private router: Router) { }
   ngOnInit(): void {
@@ -29,15 +44,17 @@ export class LoginComponent {
   }
 
   onLogin(): void {
+    this.buttonContent = 'CARGANDO...';
+    this.flagSpinner = true;
     this.authService.login(this.username, this.password).subscribe({
-      next: (response) => {
-        console.log(response);
-        
+      next: (response) => {        
         this.authService.saveToken(response.token);
         this.router.navigate(['/panel']);
       },
       error: (error) => {
         console.error("Error al iniciar sesión: ", error)
+        this.buttonContent = 'INGRESAR';
+        this.flagSpinner = false;
         this.flag = true;
         this.username = "";
         this.password = "";
