1d40302bd16a6661690a309a8d7b0c48af6b1cb9
Joseph Bano <andres.jbo2003@gmail.com>
2025-02-18 08:23:09 -0500

    Main componentes of admin view

---
+++ src/app/views/dashboard/adquisicion/adquisicion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adquisicion',
+  imports: [],
+  templateUrl: './adquisicion.component.html',
+  styleUrl: './adquisicion.component.scss'
+})
+export class AdquisicionComponent {
+
+}
