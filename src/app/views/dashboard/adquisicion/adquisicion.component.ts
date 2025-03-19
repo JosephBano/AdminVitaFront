2d75ca26a50e906c84e051dad509f89578a14bf3
Joseph Bano <andres.jbo2003@gmail.com>
2025-03-19 08:05:54 -0500

    refactor: update adquisicion component to use compras service and improve table functionality

--- src/app/views/dashboard/adquisicion/adquisicion.component.ts
+++ src/app/views/dashboard/adquisicion/adquisicion.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Column, HeadersTables } from '../../shared/util/tables';
-import { AdquisicionListResponse } from '../../../../domain/response/Adquisicion.model';
+import { CompraResponse } from '../../../../domain/response/Adquisicion.model';
 import { Table, TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
@@ -8,7 +8,8 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AdquisicionService } from '../../services/adquisicion.service';
+import { ComprasService } from '../../services/Compras.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adquisicion',
@@ -29,19 +30,20 @@ import { AdquisicionService } from '../../services/adquisicion.service';
 })
 export class AdquisicionComponent implements OnInit {
 
-  adquisicion: AdquisicionListResponse[] = [];
+  compras: CompraResponse[] = [];
   cols!: Column[];
 
   loading: boolean = true;
 
   constructor( 
-    private adquisicionService: AdquisicionService
+    private comprasService: ComprasService, 
+    private router: Router
   ){}
   ngOnInit(): void {
     this.cols = HeadersTables.AdquisicionesList;
-    this.adquisicionService.getAdquisicionList().subscribe({
+    this.comprasService.getComprasList().subscribe({
       next: (response) => {
-        this.adquisicion = response.compras;
+        this.compras = response.compras;
         this.loading = false;
       },error: (err) => {
         console.error(err);
@@ -49,23 +51,33 @@ export class AdquisicionComponent implements OnInit {
     })
   }
 
-   filterGlobal(event: Event, dt: any) { //filtro para barra de busqueda
-      const inputValue = (event.target as HTMLInputElement)?.value || '';
-      dt.filterGlobal(inputValue, 'contains');
-    }
-    clear(table: Table) {
-      table.clear();
-    }
-    formatDate(dateString: string): string {
-      if(dateString === 'Vacío') return 'Vacío';
-      const date = new Date(dateString);
-      const day = date.getDate().toString().padStart(2, '0');
-      const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Meses van de 0 a 11
-      const year = date.getFullYear();
+  filterGlobal(event: Event, dt: any) { //filtro para barra de busqueda
+    const inputValue = (event.target as HTMLInputElement)?.value || '';
+    dt.filterGlobal(inputValue, 'contains');
+  }
+  clear(table: Table) {
+    table.clear();
+  }
+  formatDate(dateString: string): string {
+    if(dateString === 'Vacío') return 'Vacío';
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
   
-      return `${day}/${month}/${year}`;
-    }
-    OnExportButton() {}
-    showDialogAdd(){}
-    showDialogEdit(){}
+    return `${day}-${month}-${year} ${hours}:${minutes}`;
+  }
+  getNameProvider(factura: string){
+    const adquisicion = this.compras.find( x => x.numeroFactura == factura);
+    if(adquisicion?.razonSocial) return `${adquisicion.nombre} ${adquisicion.razonSocial}`
+    return `${adquisicion?.nombre} ${adquisicion?.apellidos}`;
+  }
+  abrirNuevaPestana() {
+    this.router.navigate(['panel/Adquisiciones/agregar']);
+  }
+  OnExportButton() {}
+  showDialogAdd(){}
+  showDialogEdit(){}
 }
