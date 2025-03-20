a9bde25d386cb3d5150d34640c218f1f28ec8076
Joseph Bano <andres.jbo2003@gmail.com>
2025-03-20 12:53:16 -0500

    feat: implement inventory table with search and export functionality

--- src/app/views/dashboard/inventario/inventario.component.ts
+++ src/app/views/dashboard/inventario/inventario.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Column, HeadersTables } from '../../shared/util/tables';
+import { Item } from '../../../../domain/response/Item.model';
+import { ItemService } from '../../services/item.service';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Table, TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inventario',
-  imports: [],
+  imports: [
+    TableModule,
+    ButtonModule,
+    CommonModule,
+    NgFor,
+    NgIf,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.scss'
 })
-export class InventarioComponent {
+export class InventarioComponent implements OnInit {
+  Items: Item[] = [];
+  cols!: Column[];
 
+  loading: boolean = true;
+
+  constructor( 
+    private itemService: ItemService,
+  ){}
+
+  ngOnInit(): void {
+    this.cols = HeadersTables.InventarioList; 
+    this.itemService.getItemsList().subscribe({
+      next: (response) => {
+        this.Items = response;
+        this.loading = false;
+      },
+      error: (err) => console.error(err)
+    })
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
+
+  filterGlobal(event: Event, dt: any) { //filtro para barra de busqueda
+    const inputValue = (event.target as HTMLInputElement)?.value || '';
+    dt.filterGlobal(inputValue, 'contains');
+  }
 }
