d14535984bd7de0f2122064664bc9d00e86e09c1
Joseph Bano <andres.jbo2003@gmail.com>
2025-04-10 17:02:22 -0500

    feat: update routing for Usuario, Mecanico, and Proveedor components; add new HeadersTablesPersons for person data

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -22,6 +22,7 @@ export const environment = {
     conversionUnidades: '/ConversionUnidades',
     cliente: '/Clientes',
     reporte: '/Reportes',
+    usuarios: '/Usuarios',
 };
 
 export const ivaCalculo = {
