d9f49f3f760665db350884a61c6b1c5bfdc2ae74
Joseph Bano <andres.jbo2003@gmail.com>
2025-04-10 17:02:58 -0500

    feat: add CrearPersona, DeshabilitarPersona, and EditarPersona components

---
+++ src/app/views/shared/components/gestion-personas/crear-persona/crear-persona.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-persona',
+  imports: [],
+  templateUrl: './crear-persona.component.html',
+  styleUrl: './crear-persona.component.scss'
+})
+export class CrearPersonaComponent {
+
+}
