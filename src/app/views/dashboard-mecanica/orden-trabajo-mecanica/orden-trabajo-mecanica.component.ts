44b3c4902410b46e05867cb413e36308cc14684f
Joseph Bano <andres.jbo2003@gmail.com>
2025-05-01 15:45:21 -0500

    feat: implement CRUD functionality for Orden de Trabajo; add task management features and update UI components

--- src/app/views/dashboard-mecanica/orden-trabajo-mecanica/orden-trabajo-mecanica.component.ts
+++ src/app/views/dashboard-mecanica/orden-trabajo-mecanica/orden-trabajo-mecanica.component.ts
@@ -1,11 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { OrdenTrabajoService } from '../../services/orden-trabajo.service';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { TableModule, Table } from 'primeng/table';
+import { HeadersTablesMecanico } from '../../shared/util/tables';
+import { DropdownModule } from 'primeng/dropdown';
+import { EstadosVehiculo, genericT } from '../../shared/util/genericData';
+import { SelectModule } from 'primeng/select';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule } from '@angular/forms';
+import { CrudTareaMecanicaComponent } from "./crud-tarea-mecanica/crud-tarea-mecanica.component";
 
 @Component({
   selector: 'app-orden-trabajo-mecanica',
-  imports: [],
+  imports: [
+    CommonModule,
+    CardModule,
+    FormsModule,
+    TagModule,
+    ButtonModule,
+    TableModule,
+    DropdownModule,
+    SelectModule,
+    InputTextModule,
+    MultiSelectModule,
+    CrudTareaMecanicaComponent
+],
+  standalone: true,
   templateUrl: './orden-trabajo-mecanica.component.html',
   styleUrl: './orden-trabajo-mecanica.component.scss'
 })
-export class OrdenTrabajoMecanicaComponent {
+export class OrdenTrabajoMecanicaComponent implements OnInit {
+  
+  codigo: string | null = null;
 
+  OrdenTrabajo: any = null;
+
+  prioridadTexto = ['Baja', 'Media', 'Alta', 'Crítica', 'Advertencia'];
+  estadoTexto = ['Pendiente', 'Progreso', 'Finalizado'];
+  estadoVehiculoTexto = ['Operativo', 'Fuera de servicio'];
+
+  cols: any[] = [];
+  loadingGeneral: boolean = true;
+  loadingTable: boolean = true;
+  TareasOT: any[] = [];
+
+  selectedEstadoFilter!: genericT;
+  estadosFilter!: genericT[];
+  
+  agregar_tarea_card: boolean = false;
+  
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private toastr: ToastrService,
+    private ordenTrabajoService: OrdenTrabajoService,
+  ) {}
+
+  ngOnInit() {
+    this.initData();
+  }
+  
+  initData(){ 
+    this.codigo = this.route.snapshot.paramMap.get('codigo');
+    if (!this.codigo) {
+      this.toastr.error('La orden de trabajo no existe', 'Error');
+      this.router.navigate(['mecanico']);
+    }
+    this.getOrdenTrabajo(); 
+    this.cols = HeadersTablesMecanico.Tareas;
+    this.estadosFilter = EstadosVehiculo;
+  }
+  getOrdenTrabajo() {
+    this.ordenTrabajoService.getOrdenTrabajoCodigo(this.codigo!).subscribe(
+      (response) => {
+        this.OrdenTrabajo = response;
+        this.loadingGeneral = true; 
+      }
+    );
+  }
+  getSeverityEstado(status: number) {
+    switch (status) {
+      case 0: return 'success';
+      case 1: return 'warn';
+      case 2: return 'danger';
+      default:
+        return 'secondary';
+    }
+  }
+  GetEstado(id: number)  {
+    const item = this.estadosFilter.find(x => x.code === id);  
+    return item?.name;
+  }
 }
